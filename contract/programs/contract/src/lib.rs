ca467256a0f5e4e7e63540733b6d59125622af5d
Amit Kumar <amitkcodes@gmail.com>
2024-09-21 23:17:26 +0530

    updated smart contract: added the initialize function

--- contract/programs/contract/src/lib.rs
+++ contract/programs/contract/src/lib.rs
@@ -1,16 +1,40 @@
 use anchor_lang::prelude::*;
 
+pub mod constant;
+pub mod states;
+use crate::{constant::*, states::*};
+
 declare_id!("FFa222U5TqycjB4EBYF26NXMyrZHBoLNPMH842oLjHrV");
 
 #[program]
-pub mod contract {
+pub mod solana_num_flux {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        // Grabbing the user_profile as a mutable reference
+        let user_profile = &mut ctx.accounts.user_profile;
+
+        // updating the authority property of our fetched user_profile
+        user_profile.authority = ctx.accounts.authority.key();
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction()]
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+    init,
+    seeds = [USER_TAG, authority.key().as_ref()],
+    bump,
+    payer = authority,
+    space = std::mem::size_of::<UserProfile>() + 8,
+  )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    pub system_program: Program<'info, System>,
+}
