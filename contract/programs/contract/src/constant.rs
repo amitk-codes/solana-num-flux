20053fc2eb7cb5226177def1a82bb48e933b3c7b
Amit Kumar <amitkcodes@gmail.com>
2024-09-21 14:30:44 +0530

    updated smart contract: added constant.rs and states.rs files

---
+++ contract/programs/contract/src/constant.rs
@@ -0,0 +1,7 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const USER_TAG: &[u8] = b"USER_STATE";
+
+#[constant]
+pub const STORED_NUM_TAG: &[u8] = b"STORED_NUM_STATE";
---
+++ contract/programs/contract/src/states.rs
@@ -0,0 +1,14 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(Default)]
+pub struct UserProfile{
+  pub authority: Pubkey,
+}
+
+#[account]
+#[derive(Default)]
+pub struct StoredNumAccount{
+  pub authority: Pubkey,
+  pub stored_num: i64,
+}
