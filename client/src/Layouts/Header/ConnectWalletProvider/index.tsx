f90c915408084c62c65f03ffb728aef118b8e1fa
Amit Kumar <amitkcodes@gmail.com>
2024-09-21 13:17:33 +0530

    created the ConnectWalletProvider component

---
+++ client/src/Layouts/Header/ConnectWalletProvider/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter, MathWalletAdapter, LedgerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+export default function ConnectWalletProvider({ children }: { children: React.ReactNode }) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const supportedWallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter(), new MathWalletAdapter(), new LedgerWalletAdapter()], [network])
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={supportedWallets}>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
