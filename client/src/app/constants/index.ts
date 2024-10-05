5b931aa6012a49bca186f26e574102e7d24d9dca
Amit Kumar <amitkcodes@gmail.com>
2024-10-06 00:49:18 +0530

    updated client: added a new folder 'constants' and stored smart contract's id, user tag and stored num tag

---
+++ client/src/app/constants/index.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const CONTRACT_PUBKEY = new PublicKey("FFa222U5TqycjB4EBYF26NXMyrZHBoLNPMH842oLjHrV")
+
+export const USER_TAG = "USER_STATE";
+
+export const STORED_NUM_TAG = "STORED_NUM_STATE";
